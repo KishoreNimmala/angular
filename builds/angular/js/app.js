b392a7b9455af0bc710efde5cec651ff4a6a2dec
Kishore Nimmala <nkishore779@gmail.com>
2020-07-20 16:10:10 +0530

    Advanced filter with customize sorting

--- builds/angular/js/app.js
+++ builds/angular/js/app.js
@@ -4,5 +4,6 @@ myApp.controller('MyController', function MyController($scope,$http){
 	
  $http.get('js/data.json').then(function(response){
   $scope.artists=response.data;
+  $scope.artistOrder='name';
  });
 });
